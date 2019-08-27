0d023a8b8f3f32182853d4d12159f8d30066c729
Lewaa Bahmad <lewaabahmad@Lewaas-MacBook-Air.local>
2019-08-27 18:46:09 -0400

    mvp

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'main#home'
   get '/card', to: 'main#card'
+  get '/privacy-policy', to: 'main#privacy_policy'
 
   resources :messages
   resources :text_messages, :controller => 'messages'
