7a95d29e5906b6e4f9420eb6dfdc46d042181f3e
Lewaa Bahmad <lewaabahmad@Lewaas-MacBook-Air.local>
2019-08-30 16:20:07 -0400

    yer

--- app/assets/javascripts/functions.js
+++
@@ -1,4 +0,0 @@
-// document.querySelector('#card-expander').addEventListener('click', function (event) {
-//   $('#only-card').classList = ''
-//   $('#everything-else').classList = 'd-none'
-// })
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root 'main#home'
-  get '/card', to: 'main#card'
+  # get '/card', to: 'main#card'
   get '/about', to: 'main#about'
   get '/privacy-policy', to: 'main#privacy_policy'
+
+  get '/service-worker.js' => "service_worker#service_worker"
+  get '/manifest.json' => "service_worker#manifest"
 
   # resources :messages
   resources :text_messages, :controller => 'messages', only: [:create]
